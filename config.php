8549ff804930881fcc150b9fd46167e73e605dea
GlobalExpertTech <146921434+GlobalExpertTech@users.noreply.github.com>
2023-10-04 13:07:41 +0530

    update confing connection

--- config.php
+++ config.php
@@ -2,5 +2,6 @@
 <?php
 //  $con=mysqli_connect("localhost","root","","krantitrans")or die(mysqli_error($con));
  $con=mysqli_connect("localhost","u366972480_admin","zmKJO?;&Da5","u366972480_krantitrans")or die(mysqli_error($con));
+//  $con=mysqli_connect("localhost","u366972480_admin","zmKJO?;&Da5","u366972480_krantitrans")or die(mysqli_error($con));
 
 ?>
