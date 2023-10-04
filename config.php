5cee57d42167c95407ed1b494205c647a0af07a0
rahulphapale2023 <146922790+rahulphapale2023@users.noreply.github.com>
2023-10-04 13:11:28 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 
 <?php
 //  $con=mysqli_connect("localhost","root","","krantitrans")or die(mysqli_error($con));
- $con=mysqli_connect("localhost","u366972480_admin","zmKJO?;&Da5","u366972480_krantitrans")or die(mysqli_error($con));
 //  $con=mysqli_connect("localhost","u366972480_admin","zmKJO?;&Da5","u366972480_krantitrans")or die(mysqli_error($con));
+ $con=mysqli_connect("localhost","u366972480_admin","zmKJO?;&Da5","u366972480_krantitrans")or die(mysqli_error($con));
 
 ?>
